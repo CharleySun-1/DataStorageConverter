86d630a8884b712ffec1aa67b65eadf9387e48ab
CharleySun <Ysun@lcs.on.ca>
2021-02-12 12:02:16 -0500

    Added a picker to select the To unit.

--- iOS-DataStorageConverter/ContentView.swift
+++ iOS-DataStorageConverter/ContentView.swift
@@ -12,10 +12,19 @@ struct ContentView: View {
     // MARK: Stored properties
     @State private var fromUnit: DataStorageUnit = .bit
     @State private var input: String = ""
+    @State private var toUnit: DataStorageUnit = .bit
 
     // MARK: Computed properties
     private var output: String {
-        return "You typed in \(input) and selected \(fromUnit)"
+        
+        // Ensure the provided input can be expressed as an integer
+        guard let providedValue = Int(input) else {
+            return "Please provide an integer value."
+        }
+
+        // Return the converted value
+        return convert(providedValue: providedValue, fromUnit: fromUnit, toUnit: toUnit)
+
     }
     
     var body: some View {
@@ -33,6 +42,17 @@ struct ContentView: View {
 
             TextField("e.g.: 1024", text: $input)
                 .keyboardType(.numberPad)
+            
+            Picker("To unit:", selection: $toUnit) {
+                Text(DataStorageUnit.bit.rawValue).tag(DataStorageUnit.bit)
+                Text(DataStorageUnit.nibble.rawValue).tag(DataStorageUnit.nibble)
+                Text(DataStorageUnit.byte.rawValue).tag(DataStorageUnit.byte)
+                Text(DataStorageUnit.kilobyte.rawValue).tag(DataStorageUnit.kilobyte)
+                Text(DataStorageUnit.megabyte.rawValue).tag(DataStorageUnit.megabyte)
+                Text(DataStorageUnit.gigabyte.rawValue).tag(DataStorageUnit.gigabyte)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+
 
             Text(output)
         }
